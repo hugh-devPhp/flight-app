239f462347e095663c4b395bbbeb2d4c4e90fa69
Hugh <huguesody@gamail.com>
2025-01-13 10:51:59 +0000

    create datatable with angular materiel update

--- src/app/flights/components/flights-list/flights-list.component.ts
+++ src/app/flights/components/flights-list/flights-list.component.ts
@@ -3,6 +3,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule, MatTableDataSource, MatHeaderRowDef } from '@angular/material/table';
 import { Flight } from '../../../shared/models/flight.model';
+import { CommonModule } from '@angular/common';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 const ELEMENT_DATA: Flight[] = [{
   idFlight: 1, companyName: 'Air France', flightType: 'Direct flight', travelType: 'Round trip',
@@ -20,22 +22,20 @@ const ELEMENT_DATA: Flight[] = [{
   templateUrl: './flights-list.component.html',
   styleUrl: './flights-list.component.css',
   standalone: true,
-  imports: [MatTableModule, MatSortModule, MatPaginatorModule, MatHeaderRowDef,]
+  imports: [MatTableModule, MatSortModule, MatPaginatorModule, MatHeaderRowDef, CommonModule, MatCheckboxModule]
 })
 
 
 export class FlightsListComponent implements OnInit {
   displayedColumns: string[] = ['idFlight', 'companyName', 'flightType', 'travelType',
     'departureDate', 'arrivaleDate', 'backDate', 'departureLocation', 'arrivalLocation',
-    'flightDuration', 'aircraftType',];
+    'flightDuration', 'aircraftType', 'select'];
   flight = new MatTableDataSource<Flight>(ELEMENT_DATA);
 
   constructor() { }
   get flights() {
     return this.flight.data;
   }
-  ngOnInit(): void {
-    // console.log(this.flights);
-  }
+  ngOnInit(): void { '' }
 
 }
